59c00663903806d4095530be7e7250e5a4c2597b
kaiyolau <kaiyolau@gmail.com>
2023-01-06 16:22:51 -0800

    formData done

--- server/src/routers/app.js
+++ server/src/routers/app.js
@@ -7,7 +7,7 @@ const sharp = require('sharp')
 const router = new express.Router()
 const upload = multer({
     limits: {
-        fileSize: 1000000
+        fileSize: 100000000
     },
     fileFilter(req, file, cb) {
         if (!file.originalname.match(/\.(jpg|jpeg|png|pdf)$/)) {
@@ -19,22 +19,20 @@ const upload = multer({
 })
 
 
-// , upload.single('resume')
-router.post('/posts/:postingId/apps', auth, async (req, res) => {
+router.post('/posts/:postingId/apps', auth, upload.single('resume'), async (req, res) => {
     const _id = req.params.postingId
-    // console.log('req.body', req.body)
-    // console.log('req.file.buffer', req.file.buffer)
+
     try {
-        // const buffer = await sharp(req.file.buffer).resize({ width: 800, height: 1000 }).png().toBuffer()
+        const buffer = await sharp(req.file.buffer).resize({ width: 800, height: 1000 }).png().toBuffer()
         const app = new App({
             ...req.body,
-            recruiter: req.user._id,
-            job: _id
-            // resume: buffer
+            recruiter: req.user.id,
+            job: _id,
+            resume: buffer
         })
-
         await app.save()
         res.status(201).send(app)
+        console.log('sucess!')
     } catch (e) {
         res.status(400).send(e)
     }
