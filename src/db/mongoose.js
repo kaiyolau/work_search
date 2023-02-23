df28d563798dbf2b5625aaedc438325595910813
kaiyolau <kaiyolau@gmail.com>
2023-02-22 17:15:00 -0800

    updated

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/work-search-api', {
+mongoose.connect(process.env.MONGODB_URI , {
     useNewUrlParser: true
     // useCreateIndex: true,
     // useFindAndModify: false
