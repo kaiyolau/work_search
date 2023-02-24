77e1fad1787d9ba9cf5ff4ddf2c966d8c9f6fd4d
kaiyolau <kaiyolau@gmail.com>
2023-02-24 11:00:50 -0800

    changed npm verison to 18

--- seed.js
+++ seed.js
@@ -1,67 +1,67 @@
-const mongoose = require('mongoose');
-const { Faker } = require('@faker-js/faker');
+// const mongoose = require('mongoose');
+// const { faker } = require('@faker-js/faker');
 
-const faker = new Faker({ locales: { en: require('faker/locale/en') } });
+// const axios = require('axios');
 
-const axios = require('axios');
+// const { connect } = require('./src/db/mongoose'); // import your database connection
+// const { Post } = require('./src/models/post'); // import your Mongoose models
 
-const { connect } = require('./src/db/mongoose'); // import your database connection
-const { Post } = require('./src/models/post'); // import your Mongoose models
-
-const { GEOCODE_API_KEY } = require('./config/dev.env'); // import your Google Maps API key
+// const { GEOCODE_API_KEY } = require('./config/dev.env'); // import your Google Maps API key
 
 
-const data = [];
+// const data = [];
 
-async function getGeocodedAddress(city, province) {
-  try {
-    const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-      params: {
-        address: `${city}, ${province}, Canada`,
-        key: GEOCODE_API_KEY,
-      },
-    });
-    console.log(response.data);
+// async function getGeocodedAddress(city, province) {
+//   try {
+//     const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+//       params: {
+//         address: `${city}, ${province}, Canada`,
+//         key: "AIzaSyDYRXEWOptz56bZaQcaBlFp_oCmM87kEZo",
+//       },
+//     });
+//     console.log(response.data);
+//     // console.log(GEOCODE_API_KEY);
 
-    const { formatted_address, geometry: { location } } = response.data.results[0];
+//     const { formatted_address, geometry: { location } } = response.data.results[0];
 
-    return { formatted_address, latitude: location.lat, longitude: location.lng };
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
+//     return { formatted_address, latitude: location.lat, longitude: location.lng };
+//   } catch (error) {
+//     console.error(error);
+//     return null;
+//   }
+// }
 
-async function seed() {
-  const addresses = [    { city: 'Vancouver', province: 'BC' },    { city: 'Burnaby', province: 'BC' },    { city: 'Richmond', province: 'BC' },    { city: 'Vancouver', province: 'BC' },    { city: 'Burnaby', province: 'BC' },    { city: 'Richmond', province: 'BC' },    { city: 'Vancouver', province: 'BC' },    { city: 'Burnaby', province: 'BC' },    { city: 'Richmond', province: 'BC' }  ];
+// async function seed() {
+//   const addresses = [    { city: 'Vancouver', province: 'BC' },    { city: 'Burnaby', province: 'BC' },    { city: 'Richmond', province: 'BC' },    { city: 'Vancouver', province: 'BC' },    { city: 'Burnaby', province: 'BC' },    { city: 'Richmond', province: 'BC' },    { city: 'Vancouver', province: 'BC' },    { city: 'Burnaby', province: 'BC' },    { city: 'Richmond', province: 'BC' }  ];
 
-  for (const address of addresses) {
-    const { formatted_address, latitude, longitude } = await getGeocodedAddress(address.city, address.province);
+//   for (const address of addresses) {
+//     const { formatted_address, latitude, longitude } = await getGeocodedAddress(address.city, address.province);
 
-    // if (formatted_address) {
-      const post = new Post({
-        jobtitle: faker.name.jobTitle(),
-        company: faker.name.jobArea(),
-        location: faker.address.streetAddress(),
-        latitude: faker.latitude(),
-        longitude: faker.longitude(),
-        numberOfRecruiter: faker.random.number(),
-        skill: faker.name.jobArea(),
-        description: faker.lorem.sentences(3),
-        wage: faker.random.number({ min: 1000, max: 5000 }),
-        companyWebsite: faker.internet.url(),
-        sponsorship: faker.random.boolean(),
-        author: "63f6bc02c39b957f1a9e5db9"
-      });
+//     if (formatted_address) {
+//       const post = new Post({
+//         jobtitle: faker.name.jobTitle(),
+//         company: faker.name.jobArea(),
+//         //hardcoding in location
+//         location: faker.address.streetAddress(),
+//         latitude,
+//         longitude,
+//         numberOfRecruiter: 2,
+//         skill: faker.name.jobArea(),
+//         description: faker.lorem.sentences(3),
+//         wage: 15,
+//         companyWebsite: faker.internet.url(),
+//         sponsorship: true,
+//         author: "63f6bc02c39b957f1a9e5db9"
+//       });
 
-      await post.save();
-    // }
-  }
+//       await post.save();
+//     }
+//   }
 
-  console.log('Seed data created successfully!');
-}
+//   console.log('Seed data created successfully!');
+// }
 
 
-seed();
+// seed();
 
 
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,34 +1,28 @@
 const mongoose = require('mongoose')
 
-// mongoose.set('strictQuery', true);
-// mongoose.connect(process.env.MONGODB_URI , {
-//     useNewUrlParser: true
-//     // useCreateIndex: true,
-//     // useFindAndModify: false
-// })
-
-
-
-
-
-
-
-
-
 mongoose.set('strictQuery', true);
-async function connect() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    //   useCreateIndex: true,
-    //   useFindAndModify: false,
-    });
-    console.log('Connected to MongoDB!');
-  } catch (error) {
-    console.error(error);
-  }
-}
+mongoose.connect(process.env.MONGODB_URI , {
+    useNewUrlParser: true
+    // useCreateIndex: true,
+    // useFindAndModify: false
+})
 
-module.exports = { connect };
+
+
+// mongoose.set('strictQuery', true);
+// async function connect() {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//     });
+//     console.log('Connected to MongoDB!');
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// module.exports = { connect };
 
